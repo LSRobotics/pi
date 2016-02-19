f4de99deb6fdec48a25f22aaf7a4013f1969f04f
Mark Bense <markbense2016@lschs.org>
2016-02-19 17:24:15 -0500

    Added opencv files to lib folder

--- Server/server/Vision.java
+++ Server/server/Vision.java
@@ -5,6 +5,7 @@ import java.awt.image.DataBufferByte;
 import java.util.ArrayList;
 
 import org.opencv.core.Core;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
 import org.opencv.core.Point;
@@ -25,7 +26,7 @@ public class Vision {
 		byte[] pixels = ((DataBufferByte) img.getRaster().getDataBuffer()).getData();
 		Mat image_final= new Mat(img.getWidth(), img.getHeight(), CvType.CV_8UC3);
 		image_final.put(0, 0, pixels);
-		Mat rgbFiltered = RGBThresh(img, new Scalar(BGRLower), new Scalar(BGRUpper));
+		Mat rgbFiltered = RGBThresh(image_final, new Scalar(BGRLower), new Scalar(BGRUpper));
 		Mat eroded = new Mat();
 		Imgproc.erode(rgbFiltered, eroded, new Mat(), new Point(-1, -1), 2);
 		ArrayList<MatOfPoint> contours = findContours(eroded);
