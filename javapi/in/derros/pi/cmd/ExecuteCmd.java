dc67693abd8465433dcc8d36df6296bcd4e565e9
Alex Fang <frjalex@gmail.com>
2016-02-26 14:26:03 -0500

    Update ExecuteCmd.java

--- javapi/in/derros/pi/cmd/ExecuteCmd.java
+++ javapi/in/derros/pi/cmd/ExecuteCmd.java
@@ -6,8 +6,21 @@ import java.nio.*;
 
 public class ExecuteCmd {
 
-public ExecuteCmd() [
-
+public ExecuteCmd(String command) {
+            try {
+                Runtime rt = Runtime.getRuntime();
+                //Process pr = rt.exec("cmd /c dir");
+                Process pr = rt.exec(command);
+ 
+                BufferedReader input = new BufferedReader(new InputStreamReader(pr.getInputStream()));
+ 
+                String line = input.readLine();
+               
+                return line;
+            } catch(Exception e) {
+                System.out.println(e.toString());
+                e.printStackTrace();
+            }
 }
 
 }
